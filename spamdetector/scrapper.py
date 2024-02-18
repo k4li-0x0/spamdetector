e06c64042c37974282dea1492f8d48345a9ffa9e
k4li <yaroslav.sokolov05@gmail.com>
2024-02-18 14:44:10 +0300

    Fixed bug with missing dataset folder

--- spamdetector/scrapper.py
+++ spamdetector/scrapper.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, types
+from pathlib import Path
 from alive_progress import alive_bar
 import pandas as pd
 from asyncio import run
@@ -32,39 +33,46 @@ CHATS_LIST = [
 
 async def main():
     messages = []
-    async with Client('anon', api_id, api_hash) as app:
-        app: Client
-        for chat_link in CHATS_LIST:
-            chat = await app.get_chat(chat_link)  # structure with chat id
-            if chat is types.ChatPreview:
-                print(
-                    f'<TG Messages Parser> {chat_link} skipped, because it\'s private and you are not subscribed to it'
-                )
-                continue
-            print(f'<TG Messages Parser> Reading: {chat.first_name or chat.title}')
-            total = min(
-                await app.get_chat_history_count(chat.id), MAX_MESSAGES_PER_CHANNEL
-            )  # just for progress bar
-            with alive_bar(total, title=(chat.first_name or chat.title)) as bar:
-                async for message in app.get_chat_history(
-                    chat.id, MAX_MESSAGES_PER_CHANNEL
-                ):
-                    if (
-                        message.text is not None
-                    ):  # text can be None, if message is just a picture, for example
-                        messages.append(
-                            (
-                                chat.id,
-                                message.id,
-                                message.reply_to_message_id or -1,
-                                message.text,
+    try:
+        async with Client('anon', api_id, api_hash) as app:
+            app: Client
+            for chat_link in CHATS_LIST:
+                chat = await app.get_chat(chat_link)  # structure with chat id
+                if chat is types.ChatPreview:
+                    print(
+                        f'<TG Messages Parser> {chat_link} skipped, because it\'s private and you are not subscribed to it'
+                    )
+                    continue
+                print(f'<TG Messages Parser> Reading: {chat.first_name or chat.title}')
+                total = min(
+                    await app.get_chat_history_count(chat.id), MAX_MESSAGES_PER_CHANNEL
+                )  # just for progress bar
+                with alive_bar(total, title=(chat.first_name or chat.title)) as bar:
+                    async for message in app.get_chat_history(
+                        chat.id, MAX_MESSAGES_PER_CHANNEL
+                    ):
+                        if (
+                            message.text is not None
+                        ):  # text can be None, if message is just a picture, for example
+                            messages.append(
+                                (
+                                    chat.id,
+                                    message.id,
+                                    message.reply_to_message_id or -1,
+                                    message.text,
+                                )
                             )
-                        )
-                    bar()
+                        bar()
+    except Exception as e:
+        print('<TG Messages Parser>', e)
+        print('<TG Messages Parser> Saving existing messages')
+    dataset_path = Path() / 'dataset'
+    dataset_path.mkdir(exist_ok=True)
+    dataset_path /= 'messages.csv'
     df = pd.DataFrame(
         messages, columns=['chat_id', 'message_id', 'reply_id', 'message']
     )
-    df.to_csv('./dataset/messages.csv')
+    df.to_csv(dataset_path)
 
 
 run(main())
